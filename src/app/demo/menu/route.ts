40e84afa6ff3f8409c3c2226945f81b9d8cf2cac
carbolivariA <syntidev@gmail.com>
2026-05-29 20:40:16 -0400

    chore: remover demo routes temporales

--- src/app/demo/menu/route.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from 'fs'
-import { join } from 'path'
-
-export async function GET() {
-  const html = readFileSync(join(process.cwd(), 'public', 'demomenu.html'), 'utf-8')
-  return new Response(html, { headers: { 'Content-Type': 'text/html' } })
-}
